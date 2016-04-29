1ec64f5922e813f4f7fe4d89139cd510c132206f
David Fox <DavidAFox@users.noreply.github.com>
2016-04-29 15:51:45 -0500

    login format changes

--- views/login_view.php
+++ views/login_view.php
@@ -1,7 +1,11 @@
 
 <form class= "well login-form" action="index.php" method="post">
-	<div><label class="left">Name: </label><input type="text" name="name" class="right"/></div><br>
-	<div><label class="left">Password: </label><input type="password" name="password" class="right"/></div><br><br>
+	<table>
+		<tbody>
+			<tr><td><label class="left">Name: </label></td><td><input type="text" name="name" class="right"/></td></tr>
+			<tr><td><label class="left">Password: </label></td><td><input type="password" name="password" class="right"/></td></tr>
+		</tbody>
+	</table>
 	<div><input type="submit" value="submit"></div><br>
 </form>
 <div class="link-container">Click <a href="register.php">here</a> to register</div>
