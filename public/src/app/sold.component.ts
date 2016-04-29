9cab4e396136eecd58b175e99611f1057a1bc8ac
David Fox <DavidAFox@users.noreply.github.com>
2016-04-29 15:27:46 -0500

    fixed more form stuff

--- public/src/app/sold.component.ts
+++ public/src/app/sold.component.ts
@@ -13,10 +13,10 @@ import {Router} from 'angular2/router';
         <h3>{{item.name}} #{{item.id}}</h3>
         <p>{{item.description}}</p>
         <form (ngSubmit)="save()" #saleForm="ngForm">
-        <div class="form-group"><label>Handling Fee</label><input ngControl="feeControl" class="form-control" type="number" min="0" [(ngModel)]="fee" (ngModelChange)="updateFee(fee)"/></div>
+        <div class="form-group"><label>Handling Fee</label><input ngControl="feeControl" class="form-control" type="number" min="0" step=".01" [(ngModel)]="fee" (ngModelChange)="updateFee(fee)"/></div>
         <div class="form-group"><label>Quantity Sold</label><input ngControl="quantityControl" required class="form-control" type="number" min="0" [(ngModel)]="sale.quantity"/></div>
         <div class="form-group"><label>Sales Tax Rate</label><input ngControl="sTaxRateControl" required class="form-control" type="number" min="0" step=".0001"[(ngModel)]="sale.sTaxRate"/></div>
-        <div class="form-group"><label>Price</label><input ngControl="priceControl" required class="form-control" type="number" min="0" [(ngModel)]="price" (ngModelChange) = "updatePrice(price)"/></div>
+        <div class="form-group"><label>Price</label><input ngControl="priceControl" required class="form-control" type="number" min="0" step=".01" [(ngModel)]="price" (ngModelChange) = "updatePrice(price)"/></div>
         <div class="form-group"><label>Date</label><br>
             <label>M</label><input ngControl="monthControl" type="number" min="1" max="12" [(ngModel)]="month"/>
             <label>D</label><input ngControl="dayControl" type="number" min="1" max="31" [(ngModel)]="day"/>
